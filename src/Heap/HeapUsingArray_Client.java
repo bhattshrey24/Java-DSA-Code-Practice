9921b94adebe54d21691e002adf392061bda10ae
Shrey <bhatt.shrey1224@gmail.com>
2022-08-17 17:24:27 +0530

    Heapify implementation added

--- src/Heap/HeapUsingArray_Client.java
+++ src/Heap/HeapUsingArray_Client.java
@@ -3,8 +3,14 @@ package Heap;
 public class HeapUsingArray_Client {
 
     public static void main(String[] args) {
+
         HeapUsingArray_Implementation myMinHeap = new HeapUsingArray_Implementation(10);
 
+        // There are 2 ways to make heap :-
+        //1) Normally (TC - O(n(Log(n))) )
+        //2) Using heapify method (TC - O(n))
+
+        // Method 1 :- Inserting in Heap normally
         myMinHeap.insertEleInHeap(20);
         myMinHeap.insertEleInHeap(50);
         myMinHeap.insertEleInHeap(30);
@@ -14,13 +20,36 @@ public class HeapUsingArray_Client {
         myMinHeap.insertEleInHeap(80);
         myMinHeap.insertEleInHeap(70);
         myMinHeap.insertEleInHeap(90);
+
         int size = myMinHeap.currSizeOfHeap;
+
         for (int i = 0; i < size; i++) {
-            System.out.println("Element at the top : "+myMinHeap.peak());
-           boolean isSuccessful = myMinHeap.deleteElement();
-           if (isSuccessful){
-               System.out.println("Top element deleted");
-           }
+            System.out.println("Element at the top : " + myMinHeap.peak());
+            boolean isSuccessful = myMinHeap.deleteElement();
+            if (isSuccessful) {
+                System.out.println("Top element deleted");
+            }
         }
+
+        // Method 2 - Inserting in heap using Heapify method. Heapify is simply a way of creating a heap
+
+        int arrToBeHeapified[] = new int[]{10, 30, 40, 50, 70, 60, 90, 80, 20};
+
+        int heap[] = myMinHeap.makeHeapUsingHeapify(arrToBeHeapified);
+        System.out.println("Displaying heap made from heapify method");
+
+        for (int i = 0; i < heap.length; i++) {
+            System.out.println("heap ele " + i + ": " + heap[i]);
+        }
+
+        // Doing HeapSort
+        int sortedArr[] = myMinHeap.deleteAndDoHeapSort(heap); // Sorted array will be in descending
+        // order because we removed and added elements in the back of same heap array
+
+        System.out.println("Displaying Sorted heap : - ");
+        for (int ele : sortedArr) {
+            System.out.println(ele);
+        }
+
     }
 }
--- src/Heap/HeapUsingArray_Implementation.java
+++ src/Heap/HeapUsingArray_Implementation.java
@@ -1,112 +1,185 @@
 package Heap;
 
-public class HeapUsingArray_Implementation { // We are making min heap
-    int[] heapArr; // can use arrayList too but i'll use array instead
-    int currSizeOfHeap;
+public class HeapUsingArray_Implementation { // We are making MIN HEAP
+    int[] heapArr; // can use arrayList too but I'll use array instead
+    int currSizeOfHeap; //keep in mind currSizeOfHeap will always be pointing to the NEXT EMPTY
+    // SPACE in array , and if it's pointing to end of array ie. 'currSizeOfHeap == arr.length' then it means all spaces
+    // are filled . Always code heap keeping this logic in mind
 
     HeapUsingArray_Implementation(int maxCapacity) {
         heapArr = new int[maxCapacity];
-        currSizeOfHeap = 0;
-        heapArr[0] = Integer.MIN_VALUE; //0th index is not
-        // needed because then all the other properties will be violated
-        // if we start putting elements from index 0 that's why leave
-        // the index 0 with null since we can't put null in integer array therefore we put
-        // Integer.min_value
-        currSizeOfHeap++;
+        currSizeOfHeap = 0;// currently heap is empty so currSizeOfHeap is pointing to next
+        // empty space in array which is 0
     }
 
-    public int getParentIdx(int node) {
-        int idx = (node) / 2;
-        return idx;
+    public int getParentIdx(int node) { // its (node - 1) / 2 and not (node) / 2 because our heap starts
+        // from 0 and not 1 so we modified the code for getParentIdx , getLeftIdx and getRighIdx
+        return (node - 1) / 2;
     }
 
-    public int getRightIdx(int node) {
-        int idx = 2 * node + 1;
-        return idx;
+    public int getRightIdx(int node) { // will return the index of left child
+        return 2 * node + 2;
     }
 
     public int getLeftIdx(int node) {
-        int idx = 2 * node;
-        return idx;
+        return 2 * node + 1;
     }
 
-    public void insertEleInHeap(int data) { //tc- O(logn)
+    public void insertEleInHeap(int data) {
         if (currSizeOfHeap == heapArr.length) {
             System.out.println("Heap is full");
         } else {
+            // Step 1 : put new element in the last of the heap
             heapArr[currSizeOfHeap] = data;
-            rearrangeHeap();
             currSizeOfHeap++;
+
+            // Step 2: Now rearrange the heap
+            rearrangeHeap(); // have faith that this will rearrange the whole array
+            // so that it follows the min heap property ie. father is smaller than both children
         }
     }
 
     private void rearrangeHeap() {
-        int currEleIdx = currSizeOfHeap; // I have not yet increased the size therefore this will be pointing to the last element of array
-        while (heapArr[getParentIdx(currEleIdx)] > heapArr[currEleIdx]) { // this won't give index out of bound problem
-            // because we placed integer.min_value at
-            // 0th index which is technically parent of root element of heap which is at 1st index
-            // and since nothing can be smaller than -infinity therefore no swap will happen and we come out of loop safely
+        int currEleIdx = currSizeOfHeap - 1; // since currSizeOfHeap is pointing to the next
+        // empty space in array therefore last element of the heap will be at currSizeOfHeap - 1
+
+        // here we start from last and go till top
+        while (heapArr[getParentIdx(currEleIdx)] > heapArr[currEleIdx]) { // this won't give index out
+            // of bound problem because we placed because parent of node 0 is 0 itself observe the
+            // condition in while is '>' and not '>='
             int parentIdx = getParentIdx(currEleIdx);
-            swapElements(parentIdx, currEleIdx);
-            currEleIdx = parentIdx;
+            swapElements(heapArr, parentIdx, currEleIdx);
+            currEleIdx = parentIdx; // now do the same for the parent
         }
     }
 
-    public void swapElements(int i, int j) {
-        int temp = heapArr[i];
-        heapArr[i] = heapArr[j];
-        heapArr[j] = temp;
-    }
+    public boolean deleteElement() { // always the root node is deleted , tc - O(logn) , This
+        // functions returns whether deletion was successful or not
 
-    public boolean deleteElement() { // always the root node is deleted ,  tc - O(logn), This functions returns whether deletion was successful or not
-        if (currSizeOfHeap <= 1) {
+        if (currSizeOfHeap == 0) { // since currSizeOfHeap always points to
+            // the next empty space so if its pointing to first element of array means heap is empty
             System.out.println("Heap is empty can't delete");
             return false; // i.e. deletion was not successful because there is no element left to delete
         }
 
+
         //Step 1:  overwrite root element with last element of heap
-        heapArr[1] = heapArr[(currSizeOfHeap - 1)];
+        int lastElementsIdx = currSizeOfHeap - 1;
+        heapArr[0] = heapArr[(lastElementsIdx)];
 
-        //Step 2:  delete last element by putting infinity
-        heapArr[(currSizeOfHeap - 1)] = Integer.MAX_VALUE;
+        //Step 2:  delete last element by putting infinity in it
+        heapArr[(lastElementsIdx)] = Integer.MAX_VALUE;
 
-        // Step 3: rearrange from top to bottom so that it follows the heap property
-        int currNodeIdx = 1;
+        // Step 3: rearrange from TOP to BOTTOM so that it follows the heap property
+        int currNodeIdx = 0;
+
         while (currSizeOfHeap > currNodeIdx) {
 
             int currNodeValue = heapArr[currNodeIdx];
             int currNodeLeftChildIdx = getLeftIdx(currNodeIdx);
             int currNodeRightChildIdx = getRightIdx(currNodeIdx);
 
-            if (currNodeLeftChildIdx < currSizeOfHeap && currNodeRightChildIdx < currSizeOfHeap) {
+            if (currNodeLeftChildIdx < currSizeOfHeap && currNodeRightChildIdx < currSizeOfHeap) { // if current node has both children
                 int currNodeLeftChildValue = heapArr[currNodeLeftChildIdx];
                 int currNodeRightChildValue = heapArr[currNodeRightChildIdx];
 
                 int smallerChildsIdx = currNodeLeftChildValue > currNodeRightChildValue ? currNodeRightChildIdx : currNodeLeftChildIdx;
 
                 if (heapArr[currNodeIdx] > heapArr[smallerChildsIdx]) {
-                        swapElements(smallerChildsIdx, currNodeIdx);
-                        currNodeIdx = smallerChildsIdx;
+                    swapElements(heapArr, smallerChildsIdx, currNodeIdx);
+                    currNodeIdx = smallerChildsIdx;
                 } else {
-                        break;
+                    break;
                 }
-            } else if (currNodeLeftChildIdx < currSizeOfHeap && heapArr[currNodeLeftChildIdx] < currNodeValue) {
-                swapElements(currNodeLeftChildIdx, currNodeIdx);
+            } else if (currNodeLeftChildIdx < currSizeOfHeap && heapArr[currNodeLeftChildIdx] < currNodeValue) {// if current node has just left child
+                swapElements(heapArr, currNodeLeftChildIdx, currNodeIdx);
                 currNodeIdx = currNodeLeftChildIdx;
-            } else if (currNodeRightChildIdx < currSizeOfHeap && heapArr[currNodeRightChildIdx] < currNodeValue) {
-                swapElements(currNodeRightChildIdx, currNodeIdx);
+
+            } else if (currNodeRightChildIdx < currSizeOfHeap && heapArr[currNodeRightChildIdx] < currNodeValue) {// if current node has just right child
+                swapElements(heapArr, currNodeRightChildIdx, currNodeIdx);
                 currNodeIdx = currNodeRightChildIdx;
-            } else {
+
+            } else {// If current node is leaf then don't do anything simply break out of loop
                 break;
             }
         }
-        currSizeOfHeap--;
+
+        currSizeOfHeap--; // decrease since we removed one element
+
         return true;
     }
 
     public int peak() {
-        return heapArr[1];
+        return heapArr[0];
     }
 
+    public void swapElements(int arr[], int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public int[] makeHeapUsingHeapify(int arr[]) {
+        // In heapify we start from last element and
+        // make sure all the elements below it are following heap property
+
+        int startNode = (arr.length / 2) + 1; // we don't process leaf node since
+        // they are already heap so (arr.length / 2) + 1 will take us to the
+        // first non leaf node from last of array
+
+        for (int i = startNode; i > 0; i--) { // we heapify elements till we reach top
+            heapify(arr, arr.length - 1, i);
+        }
+
+        return arr;
+    }
+
+    public void heapify(int arr[], int lengthOfArr, int node) {
+        int leftChildIdx = getLeftIdx(node);
+        int rightChildIdx = getRightIdx(node);
+        int smallestNodeIdx = node; // currently we are assuming that current node
+        // is the smallest among the three ie. current node it's left child and its' right child
+
+        // this function is doing similar work as deleteElement() but in a much cleaner and beautiful way
+        if (leftChildIdx <= lengthOfArr && arr[smallestNodeIdx] > arr[leftChildIdx]) {
+            smallestNodeIdx = leftChildIdx;// this 'if' check and below 'if' check will
+            // simply find the smallest of the three ie. current node its left child and its right child
+        }
+        if (rightChildIdx <= lengthOfArr && arr[smallestNodeIdx] > arr[rightChildIdx]) { // remember that
+            // right now 'smallestNodeIdx' contains the smaller of the 2 ie current node and its left child
+            smallestNodeIdx = rightChildIdx;
+        }
+
+        if (smallestNodeIdx != node) { // i.e. we only swap and heapify
+            // further if any of the 2 children of current node is smaller than he is
+            // otherwise don't do anything simply return
+
+            swapElements(arr, node, smallestNodeIdx);
+            heapify(arr, lengthOfArr, smallestNodeIdx); // Have leap of faith that this
+            // function will heapify further
+        }
+    }
+
+    public int[] deleteAndDoHeapSort(int heap[]) {
+        int lastEleOfHeap = heap.length - 1;
+
+        for (int i = 0; i < heap.length - 1; i++) {
+            swapElements(heap, lastEleOfHeap, 0); // we are creating the sorted heap
+            // in the same heapArray by simply putting the deleted element in the last
+
+            lastEleOfHeap -= 1; // now we reduced the size of heap since we deleted 1 element from it
+
+            heapify(heap, lastEleOfHeap, 0); // now have faith that this
+            // will heapify the remaining heap. And observe this won't hinder
+            // the sorted array that we are building in the end because
+            // understand how heapify works : heapify(a, b, c) function says that it's
+            // heapifying the heap 'a' who's length is 'b' so 'b' is basically the size of the heap
+            // and since we are decreasing the heap size at every iteration by doing 'lastEleOfHeap -= 1;'
+            // therefore heapify will only affect the heap and not our sorted array
+        }
+        return heap;
+    }
 
 }
+
+
